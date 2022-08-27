6515a5ef8ac4086351b3fe9ec26b760dd28fb2e4
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-08-27 18:53:37 -0400

    Initialized tailwind css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1 className='text-3xl font-bold underline'>
+        Hello World
+      </h1>
+    </div>
   )
 }
 

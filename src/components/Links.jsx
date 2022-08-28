830d73e55c7221ff0f77924c17a0f709677af5c6
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-08-28 19:09:24 -0400

    Added functionality to search bar

---
+++ src/components/Links.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const links = [
+    { url: '/search', text: '🔍 All'},
+    { url: '/news', text: '📰 News'},
+    { url: '/image', text: '🖼️ Images'},
+    { url: '/videos', text: '📺 Videos'},
+];
+
+const Links = () => {
+  return (
+    <div className='flex sm:justify-around justify-between items-center mt-4'>
+        {links.map(({ url, text }) => 
+            <NavLink to={url} className={({isActive}) => isActive ? "text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2 m-2 mb-2" : "m-2 mb-2" } >
+                {text}
+            </NavLink>
+        )}
+    </div>
+  )
+}
+
+export default Links
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDebounce } from 'use-debounce'
+import { useResultContext } from '../context/ResultContextProvider'
+import Links from './Links'
 
 const Search = () => {
+
+  const [text, setText] = useState('Java');
+  const { setSearchTerm } = useResultContext();
+  const [ debouncedValue ] = useDebounce(text, 300);
+
+  useEffect(() => {
+    if(debouncedValue) setSearchTerm(debouncedValue)
+  }, [debouncedValue])
+
   return (
-    <div>
-      Search
+    <div className='relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3'>
+      <input value={text} type='text' className='sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg' placeholder='Search here!' onChange={(e) => setText(e.target.value)}/>
+      {!text && (
+        <button type='button' className='absolute top-1.5 right-4 text-2xl text-gray-500' onClick={() => setText('')}>
+          X
+        </button>
+      )}
+      <Links />
     </div>
   )
 }

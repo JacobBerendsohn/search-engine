89e5b07af30ac439358de701c16e19920623f43d
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-08-28 18:21:49 -0400

    Solved issue where API used entries as a type, so changed data layering to not have to call a JS function for data

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -26,7 +26,7 @@ const Results = () => {
     case '/search':
       return (
         <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
-          {results?.results?.map(({ link, title }, index) => (
+          {results?.map(({ link, title }, index) => (
             <div key={index} className='md:w-2/5 w-full'>
               <a href={link} target='_blank' rel='noreferrer'>
                 <p className='text-sm'>
@@ -43,7 +43,7 @@ const Results = () => {
     case '/image':
       return (
         <div className='flex flex-wrap justify-center items-center'>
-          {results?.image_results?.map(({ image, link: { href, title }}, index) => (
+          {results?.map(({ image, link: { href, title }}, index) => (
             <a className='sm:p-3 p-5' href={href} key={index} target='_blank' rel='noreferrer'>
               <img src={image?.src} alt={title} loading='lazy' />
               <p className='w-36 break-words text-sm mt-2'>
@@ -54,7 +54,24 @@ const Results = () => {
         </div>
       )
     case '/news':
-      return 'NEWS'
+      return (
+        <div className='flex flex-wrap justify-between space-y-6 sm:px-56 items-center'>
+          {results?.map(({ links, id, source, title }) => (
+            <div key={id} className='md:w-2/5 w-full'>
+              <a href={links?.[0].href} target='_blank' rel='noreferrer' className='hover:underline'>
+                <p className='text-lg dark:text-blue-300 text-blue-700'>
+                  {title}
+                </p>
+                <div className='flex gap-4'>
+                  <a href={source?.href} target='_blank' rel='noreferrer'>
+                    {source?.href}
+                  </a>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      )
     case '/videos':
       return 'VIDEOS'
     default:
